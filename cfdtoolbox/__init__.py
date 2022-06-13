904afa44cee2cb869d8a1725ad6d49d507445119
Matthias Probst <matth.probst@gmail.com>
2022-06-13 12:31:10 +0200

    update _write_dot_env_file
    There should be a win and linux env file

--- cfdtoolbox/__init__.py
+++ cfdtoolbox/__init__.py
@@ -1,9 +1,13 @@
 import pathlib
 
 from ._logger import logger, _file_handler, _stream_handler
-
+import platform
 USER_CONFIG_DIR = pathlib.Path.home() / ".config" / __package__
-CFX_DOTENV_FILENAME = USER_CONFIG_DIR.joinpath('cfx.env')
+
+if platform.system() == 'Windows':
+    CFX_DOTENV_FILENAME = USER_CONFIG_DIR.joinpath('cfx_win.env')
+else:
+    CFX_DOTENV_FILENAME = USER_CONFIG_DIR.joinpath('cfx_linux.env')
 SESSIONS_DIR = pathlib.Path(__file__).parent.joinpath('cfx/session_files')
 
 if not SESSIONS_DIR.exists():
@@ -22,20 +26,22 @@ def _make_user_config_dir():
     if not USER_CONFIG_DIR.exists():
         USER_CONFIG_DIR.mkdir()
 
+
 def _make_sessions_dir():
     """creates the tmp directory for cfx session files"""
     if not SESSIONS_DIR.exists():
         SESSIONS_DIR.mkdir()
 
 
-def _write_dot_env_file(overwrite=False):
+def _write_dot_env_file(inst_dir, overwrite=False):
     """creates the .env file for cfx environment variables"""
     if not overwrite and CFX_DOTENV_FILENAME.exists():
         raise FileExistsError('cfx.env file already exists in user directory')
 
     with open(CFX_DOTENV_FILENAME, 'w') as f:
-        f.write('cfx5pre = /opt/dist/ansys_inc/v202/CFX/bin/cfx5pre\n')
-        f.write('cfx5solve = /opt/dist/ansys_inc/v202/CFX/bin/cfx5solve\n')
-        f.write('cfx5stop = /opt/dist/ansys_inc/v202/CFX/bin/cfx5stop\n')
-        f.write('cfx5cmds = /opt/dist/ansys_inc/v202/CFX/bin/cfx5cmds\n')
-        f.write('cfx5mondata = /opt/dist/ansys_inc/v202/CFX/bin/cfx5mondata')
+        f.write(f'cfx5instdir = {inst_dir}\n')  # C:\Program Files\ANSYS Inc\v202\CFX\bin
+        f.write(f'cfx5pre = {inst_dir}/bincfx5pre\n')
+        f.write(f'cfx5solve = {inst_dir}/bincfx5solve\n')
+        f.write(f'cfx5stop = {inst_dir}/bincfx5stop\n')
+        f.write(f'cfx5cmds = {inst_dir}/bincfx5cmds\n')
+        f.write(f'cfx5mondata = {inst_dir}/bincfx5mondata')
